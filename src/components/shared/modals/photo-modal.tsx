21eea0a8b6870887167222301be723b0dadb1472
EliasSahlos <esahlos12@gmail.com>
2024-07-02 19:33:01 +0300

    Added title for photo-modal

--- src/components/shared/modals/photo-modal.tsx
+++ src/components/shared/modals/photo-modal.tsx
@@ -1,5 +1,5 @@
 // /src/components/shared/modals/photo-modal.tsx
-import { Box, IconButton, Modal } from '@mui/material';
+import { Box, IconButton, Modal, Stack, Typography } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import { useState } from 'react';
 import { RootState } from '../../../state/store';
@@ -24,6 +24,7 @@ const style = {
 function PhotoModal() {
     const [objectFit, setObjectFit] = useState<'cover' | 'contain'>('cover');
 
+    const user: boolean = false;
     const dispatch = useDispatch();
     const isPhotoModalOpen: boolean = useSelector((state: RootState) => state.photoModal.isOpen);
     const selectedItem: IPhoto | undefined = useSelector((state: RootState) => state.photoModal.selectedItem);
@@ -61,26 +62,44 @@ function PhotoModal() {
                     </IconButton>
                 </Box>
                 {selectedItem && (
-                    <Box
-                        display='flex'
-                        flexDirection='column'
-                        justifyContent='center'
-                        alignItems='center'
-                        height='100%'
-                        width='100%'
-                    >
-                        <img
-                            src={selectedItem.url}
-                            alt={selectedItem.title}
-                            style={{
-                                height: '100%',
-                                width: '100%',
-                                objectFit: objectFit,
-                                borderRadius: '4px',
-                            }}
-                            onLoad={handleImageLoad}
-                        />
-                    </Box>
+                    <>
+                        <Box
+                            display='flex'
+                            flexDirection='column'
+                            justifyContent='center'
+                            alignItems='center'
+                            height='calc(100% - 40px)'
+                            width='100%'
+                        >
+                            <img
+                                src={selectedItem.url}
+                                alt={selectedItem.title}
+                                style={{
+                                    height: '100%',
+                                    width: '100%',
+                                    objectFit: objectFit,
+                                    borderRadius: '4px 4px 0px 0px',
+                                }}
+                                onLoad={handleImageLoad}
+                            />
+                        </Box>
+                        <Box
+                            width='100%'
+                            bgcolor='white'
+                            py={1}
+                            gap={2}
+                            borderRadius='4px'
+                        >
+                            <Stack display='flex' justifyContent='center' alignItems='center' direction='row'>
+                                <Typography>{selectedItem.title}</Typography>
+                                {user && (
+                                    <Typography mx={1} fontWeight={500}>
+                                        [ {selectedItem.number} ]
+                                    </Typography>
+                                )}
+                            </Stack>
+                        </Box>
+                    </>
                 )}
             </Box>
         </Modal>
