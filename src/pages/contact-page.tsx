ff2d69694b0f47000b52c7d4884a04086b7987c7
IliasSachlos <esahlos12@gmail.com>
2024-08-19 13:37:41 +0300

    Design changes

--- src/pages/contact-page.tsx
+++ src/pages/contact-page.tsx
@@ -1,38 +1,42 @@
-import { Box, Container, Grid } from "@mui/material";
-import ProfileImage from "../components/contact/profile-image";
-import ContactCard from "../components/contact/contact-card";
+import { Box, Container, Grid } from '@mui/material';
+import ProfileImage from '../components/contact/profile-image';
+import ContactCard from '../components/contact/contact-card';
 
 function ContactPage() {
     return (
-        <Container maxWidth="xl">
-            <Box display="flex" justifyContent="center" alignItems="center" height="90vh">
-                <Grid container height="100%" p={1}>
+        <Container maxWidth='xl'>
+            <Box display='flex' justifyContent='center' alignItems='center' height='90vh'>
+                <Grid container height='100%' p={1}>
                     <Grid
-                        display="flex"
-                        justifyContent="center"
-                        alignItems="center"
                         item
+                        display='flex'
+                        justifyContent='center'
+                        alignItems='center'
                         xs={12}
                         md={6}
-                        textAlign="center"
-                        my={2}
-                        data-aos="zoom-in-up"
-                        data-aos-duration="800"
-                        data-aos-once="true"
+                        textAlign='center'
+                        data-aos='zoom-in-up'
+                        data-aos-duration='800'
+                        data-aos-once='true'
                     >
                         <ProfileImage />
                     </Grid>
                     <Grid
                         item
+                        display='flex'
+                        justifyContent='center'
+                        alignItems='center'
                         xs={12}
                         md={6}
-                        textAlign="center"
-                        data-aos="zoom-in-up"
-                        data-aos-delay="200"
-                        data-aos-duration="800"
-                        data-aos-once="true"
+                        textAlign='center'
+                        data-aos='zoom-in-up'
+                        data-aos-delay='200'
+                        data-aos-duration='800'
+                        data-aos-once='true'
                     >
-                        <ContactCard />
+                        <Box mb={{ md: 15, xl: 25 }}>
+                            <ContactCard />
+                        </Box>
                     </Grid>
                 </Grid>
             </Box>
