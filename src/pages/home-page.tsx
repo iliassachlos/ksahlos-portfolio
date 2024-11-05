979b7d2fa63ddfd0adbca327603ecf3769810fa9
IliasSachlos <esahlos12@gmail.com>
2024-11-05 20:41:42 +0200

    Changed homepage photo

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,33 +1,34 @@
-import { Box, Container, Stack, Typography } from "@mui/material";
-import { TypeAnimation } from "react-type-animation";
+import { Box, Container, Stack, Typography } from '@mui/material';
+import { grey } from '@mui/material/colors';
+import { TypeAnimation } from 'react-type-animation';
 
 function HomePage() {
     const homepagePhoto: string =
-        "https://firebasestorage.googleapis.com/v0/b/ksahlos-portfolio.appspot.com/o/others%2Fswan.jpg?alt=media&token=28b0d3c2-a291-4a03-a985-787a520cd61d";
+        'https://firebasestorage.googleapis.com/v0/b/ksahlos-portfolio.appspot.com/o/others%2Friver-home.jpeg?alt=media&token=1ff75443-24b2-44fe-9342-735485e1396a';
     return (
-        <Box position="relative" zIndex={-1} width="100%" height="100%">
+        <Box position='relative' zIndex={-1} width='100%' height='100%'>
             <img
-                style={{ objectFit: "cover", width: "100%", height: "90vh" }}
+                style={{ objectFit: 'cover', width: '100%', height: '90vh' }}
                 src={homepagePhoto}
-                alt="homepage-photo"
-                loading="eager"
+                alt='homepage-photo'
+                loading='eager'
             />
             <Container>
-                <Box position="absolute" display="flex" justifyContent="center" alignItems="center" sx={{ inset: 0 }}>
-                    <Stack direction="column" mb={6} spacing={1}>
-                        <Typography variant="h1" fontSize={25} color="white"  data-aos="zoom-in-up">
+                <Box position='absolute' display='flex' justifyContent='center' alignItems='center' sx={{ inset: 0 }}>
+                    <Stack direction='column' mb={6} spacing={1}>
+                        <Typography variant='h1' fontSize={25} fontWeight={500} color={grey[700]} data-aos='zoom-in-up'>
                             Fine - Art Photography
                         </Typography>
                         <Box
-                            textAlign="center"
-                            fontStyle="italic"
-                            color="white"
+                            textAlign='center'
+                            fontStyle='italic'
+                            fontWeight={500}
+                            color={grey[600]}
                             fontSize={12}
-                           
-                            data-aos="zoom-in-up"
-                            data-aos-delay="100"
+                            data-aos='zoom-in-up'
+                            data-aos-delay='100'
                         >
-                            <TypeAnimation sequence={["Timeless steps beyond reality"]} speed={30} cursor={false} />
+                            <TypeAnimation sequence={['Timeless steps beyond reality']} speed={30} cursor={false} />
                         </Box>
                     </Stack>
                 </Box>
