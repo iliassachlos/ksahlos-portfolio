b32c3b51f49bddf3cc01513e6fbc5d6b9993e016
IliasSachlos <esahlos12@gmail.com>
2024-12-03 17:11:05 +0200

    Route minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,40 @@
-import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/home-page";
-import NotFoundPage from "./pages/not-found-page";
-import AboutPage from "./pages/about-page";
-import PricingPage from "./pages/pricing-page";
-import ContactPage from "./pages/contact-page";
-import EscapePage from "./pages/fine-art/escape-page";
-import EtherialPage from "./pages/fine-art/etherial-page";
-import IllusionPage from "./pages/fine-art/illusion-page";
-import LoginPage from "./pages/login-page";
-import AdminpanelPage from "./pages/admin-page";
-import ExhibitsPage from "./pages/exhibits-page";
-import LocalArtPage from "./pages/local-art/local-art-page";
-import EssentialPage from "./pages/fine-art/essential-page";
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/home-page';
+import NotFoundPage from './pages/not-found-page';
+import AboutPage from './pages/about-page';
+import PricingPage from './pages/pricing-page';
+import ContactPage from './pages/contact-page';
+import EscapePage from './pages/fine-art/escape-page';
+import EtherialPage from './pages/fine-art/etherial-page';
+import IllusionPage from './pages/fine-art/illusion-page';
+import LoginPage from './pages/login-page';
+import AdminpanelPage from './pages/admin-page';
+import ExhibitsPage from './pages/exhibits-page';
+import LocalArtPage from './pages/local-art/local-art-page';
+import EssentialPage from './pages/fine-art/essential-page';
 
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/pricing" element={<PricingPage />} />
-            <Route path="/exhibits" element={<ExhibitsPage />} />
+            {/* Pages */}
+            <Route path='/' element={<HomePage />} />
+            <Route path='/about' element={<AboutPage />} />
+            <Route path='/contact' element={<ContactPage />} />
+            <Route path='/pricing' element={<PricingPage />} />
+            <Route path='/exhibits' element={<ExhibitsPage />} />
 
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/admin" element={<AdminpanelPage />} />
+            {/* Gallery Pages */}
+            <Route path='/fine-art/escape' element={<EscapePage />} />
+            <Route path='/fine-art/illusion' element={<IllusionPage />} />
+            <Route path='/fine-art/etherial' element={<EtherialPage />} />
+            <Route path='/fine-art/essential' element={<EssentialPage />} />
+            <Route path='/local-art' element={<LocalArtPage />} />
 
-            <Route path="/fine-art/escape" element={<EscapePage />} />
-            <Route path="/fine-art/illusion" element={<IllusionPage />} />
-            <Route path="/fine-art/etherial" element={<EtherialPage />} />
-            <Route path="/fine-art/essential" element={<EssentialPage />} />
-            <Route path="/local-art" element={<LocalArtPage />} />
+            {/* Admin Pages */}
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/admin' element={<AdminpanelPage />} />
 
-            <Route path="*" element={<NotFoundPage />} />
+            <Route path='*' element={<NotFoundPage />} />
         </Routes>
     );
 }
