707c1fa6fccdfe498b160f424672309c19b6628f
IliasSachlos <esahlos12@gmail.com>
2025-03-16 21:09:53 +0200

    env changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,14 +5,14 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
-    apiKey: "AIzaSyAf29Vdab4GWrg6ByUGOLvRhyofn8Xpo_I",
-    authDomain: "ksahlos-portfolio.firebaseapp.com",
-    projectId: "ksahlos-portfolio",
-    storageBucket: "ksahlos-portfolio.appspot.com",
-    messagingSenderId: "244233584665",
-    appId: "1:244233584665:web:fa476ddced77f2353fea88",
-    measurementId: "G-F6NMRCFTXY"
-  };
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
