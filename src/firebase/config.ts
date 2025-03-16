16eed4f529f491899912508b5b01b033c48feace
IliasSachlos <esahlos12@gmail.com>
2025-03-16 22:43:08 +0200

    fixes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -14,7 +14,7 @@ const firebaseConfig = {
     measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore();
+
+export const db = getFirestore(app);
 export const auth = getAuth(app);
